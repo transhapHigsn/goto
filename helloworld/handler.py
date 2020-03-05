b6c1a00935a80878ca2d3c196bf2a2fe9ee8439b
transhap <higsn>
2020-03-06 02:24:02 +0530

    changes to the handler

--- helloworld/handler.py
+++ helloworld/handler.py
@@ -1,7 +1,13 @@
+# 178.128.40.109
+import json
+
 def handle(req):
     """handle a request to the function
     Args:
         req (str): request body
     """
 
-    return "Hello World! from Higsn Openfaas."
+    body = json.dumps(req)
+    nex = json.loads(req)
+
+    return body, nex
